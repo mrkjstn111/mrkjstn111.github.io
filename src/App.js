42cf36ba5837b7b248b1829af71987269245ddb2
Justin Dinglasan <88678036+mrkjstn111@users.noreply.github.com>
2024-02-11 11:25:34 +0800

    Creating pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './App.css';
+import { Homepage } from "./Pages";
+import Navbar from "./Components/Navbar";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <React.StrictMode>
+          <Navbar/>
+          <RouterProvider router={router} />
+        </React.StrictMode>
     </div>
   );
 }
---
+++ src/Pages/index.js
@@ -0,0 +1,10 @@
+
+export function Homepage () {
+    return(
+        <div className="flex flex-col sm:min-h-screen items-center justify-center">
+            <div className=" text-center">
+                <h1 className="text-xl animate-pulse">Hello World</h1>
+            </div>
+        </div>
+    )
+}
